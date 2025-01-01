a942be838abfc3b25119351722228dce47c4fb49
Ruyu <119630026+Ruy-u@users.noreply.github.com>
2025-01-01 18:39:30 +0300

    Push to github

--- ShoppingCartApp/ShoppingCartApp/ShoppingCartApp/Helpers/SessionHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShoppingCartApp.Helpers
-{
-    public static class SessionHelper
-    {
-        public static string LoggedInUserName { get; set; } // Holds the username
-        public static int LoggedInUserId { get; set; } // Holds the user ID
-    }
-}
--- ShoppingCartApp/ShoppingCartApp/ShoppingCartApp/Helpers/SharedCart.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShoppingCartApp.Models;
-
-namespace ShoppingCartApp.Helpers
-{
-    public static class SharedCart
-    {
-        public static List<ItemProjects> CartItems { get; } = new List<ItemProjects>();
-    }
-}
